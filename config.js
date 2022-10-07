2b5a3a5b9158c76b9415a554895b0f47b9ffd77b
Fafago1k <113995015+Fafago1k@users.noreply.github.com>
2022-10-07 17:20:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -372,8 +372,8 @@ global.hsquere = ['⛶','❏','⫹⫺']
 /*============== STICKER WM ==============*/
 global.stickpack = '.'
 global.stickauth = `☂︎\n𝗘\nl\na\ni\nn\na\n-\n𝗕\n𝗢\n𝗧\n✦\n\n⫹⫺ Whatsapp BOT\nwa.me/${global.nomorbot}`
-global.packname = 'Cʀᴇᴀᴛᴇᴅ Bʏ'
-global.packname2 = 'ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴇʟᴀɪɴᴀ ʙᴏᴛᴢ'
+global.packname = 'CastwlMawonBot'
+global.packname2 = 'by©Fafago1k'
 
 global.multiplier = 38 // The higher, The harder levelup
 
